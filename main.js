0f1662f30410af1a77cf64750cd174353336ffd3
naufalalifai <naufalalifmanunited@gmail.com>
2022-09-13 12:55:52 +0700

    add vertex and fragment shader

---
+++ main.js
@@ -0,0 +1,16 @@
+function main() {
+    var canvas = document.getElementById("myCanvas");
+    var gl = canvas.getContext("webgl");
+
+    // VERTEX SHADER
+    var vertexShaderCode =
+    "void main () {" +
+    "}";
+
+    // FRAGMENT SHADER
+    var fragmentShaderCode = `
+        void main() {
+            
+        }
+    `;
+}
