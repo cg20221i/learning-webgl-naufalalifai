c9d7def45f6bc0db3b82c7eb3f51b410d3d5b680
naufalalifai <naufalalifmanunited@gmail.com>
2022-09-13 12:59:57 +0700

    pink canvas

--- main.js
+++ main.js
@@ -6,11 +6,31 @@ function main() {
     var vertexShaderCode =
     "void main () {" +
     "}";
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderCode);
+    gl.compileShader(vertexShader);
 
     // FRAGMENT SHADER
     var fragmentShaderCode = `
         void main() {
-            
+
         }
     `;
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderCode);
+    gl.compileShader(fragmentShader);
+
+    // Comparing to C-Programming, we may imagine
+    //  that up to this step we have created two
+    //  object files (.o), for the vertex and fragment shaders
+
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    gl.clearColor(1.0, 0.75,   0.79,  1.0);
+                //Red, Green, Blue, Alpha
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
