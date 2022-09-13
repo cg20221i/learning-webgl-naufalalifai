efe677770c1d9ff42e729c325b7ceb477efc23a3
naufalalifai <naufalalifmanunited@gmail.com>
2022-09-13 14:06:14 +0700

    draw point

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ function main() {
     var gl = canvas.getContext("webgl");
 
     // VERTEX SHADER
-    var vertexShaderCode =
-    "void main () {" +
-    "}";
+    var vertexShaderCode = `
+        void main () {
+            gl_PointSize = 10.0;
+            gl_Position = vec4(0.5, 0.5, 1.0, 1.0);
+            // gl_Position is the final destination for storing
+            //  positional data for the rendered vertex
+        }
+    `;
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
@@ -13,7 +18,9 @@ function main() {
     // FRAGMENT SHADER
     var fragmentShaderCode = `
         void main() {
-
+            gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+            // gl_FlagColor is the final destination for storing
+            //  color data for the rendered fragment
         }
     `;
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -33,4 +40,6 @@ function main() {
     gl.clearColor(1.0, 0.75,   0.79,  1.0);
                 //Red, Green, Blue, Alpha
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINT, 0, 1);
 }
